3b7dd2c983263ef7ac2fecb59258e19f0577eb81
Rana Akbas <akbasnar@gmail.com>
2024-07-10 16:19:02 +0300

    base-files

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  bracketSpacing: false, //no spaces in curly brackets
+  jsxBracketSameLine: true, //close tag are in the same line with action
+  singleQuote: false, // use ""
+  trailingComma: "none", //last elemet has no comma
+  printWidth: 120, //line width
+  tabWidth: 2, //tab space width
+  arrowParens: "avoid" //do not use brackets at one element operations
+};
