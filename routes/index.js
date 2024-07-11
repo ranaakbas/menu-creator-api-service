cc21ed856da8817dd881f7563ec25ba3bda56ab8
Rana Akbas <akbasnar@gmail.com>
2024-07-11 16:09:23 +0300

    categoryListCreate

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const MenuItemRoutes = require("./MenuItemRoutes");
+const CategoryRoutes = require("./CategoryRoutes");
 
 router.use("/menu-items", MenuItemRoutes);
+router.use("/categories", CategoryRoutes);
 
 module.exports = router;
