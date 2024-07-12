b57bac8e1e8a9f314811ca2d7b74815f6ba707bd
Rana Akbas <akbasnar@gmail.com>
2024-07-12 21:47:00 +0300

    redone categoryDetail

--- routes/CategoryRoutes.js
+++ routes/CategoryRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const Category = require("../models/CategoryModel");
-const MenuItemCategory = require("../models/MenuItemCategoryModel");
 const MenuItem = require("../models/MenuItemModel");
 
 router.get("/", async (req, res) => {
