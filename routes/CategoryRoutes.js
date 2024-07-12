7d091dc23eb6497d2dc1fa37f47137cfaaa3ffce
Rana Akbas <akbasnar@gmail.com>
2024-07-12 21:46:33 +0300

    categoryDetail

--- routes/CategoryRoutes.js
+++ routes/CategoryRoutes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const Category = require("../models/CategoryModel");
+const MenuItemCategory = require("../models/MenuItemCategoryModel");
+const MenuItem = require("../models/MenuItemModel");
 
 router.get("/", async (req, res) => {
   try {
@@ -24,6 +26,57 @@ router.post("/", async (req, res) => {
     }
     const newCategory = await Category.create({name});
     return res.json(newCategory);
+  } catch (error) {
+    return res.status(400).json({errors: [error.message]});
+  }
+});
+
+router.get("/:id/items", async (req, res) => {
+  try {
+    const {id} = req.params.id;
+
+    const items = await MenuItem.find({category: id, isDeleted: false});
+    return res.json(items);
+  } catch (error) {
+    return res.status(400).json({errors: [error.message]});
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const {id} = req.params;
+    const {name} = req.body;
+
+    if (!name) {
+      return res.status(400).json({errors: ["name is required"]});
+    }
+    const existingCategory = await Category.findOne({name: name});
+    if (existingCategory && existingCategory._id.toString() !== id) {
+      return res.status(400).json({errors: "already exists"});
+    }
+    const category = await Category.findById(id);
+    if (!category) {
+      return res.status(404).json({message: "category not found"});
+    }
+
+    const updatedCategory = await Category.findByIdAndUpdate(id, {name: name}, {new: true});
+    return res.json(updatedCategory);
+  } catch (error) {
+    return res.status(400).json({errors: [error.message]});
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const {id} = req.params;
+
+    const category = await Category.findById(id);
+    if (!category) {
+      return res.status(400).json({errors: "category not found"});
+    }
+
+    await Category.findByIdAndDelete(id);
+    return res.json({message: "category deleted successfully"});
   } catch (error) {
     return res.status(400).json({errors: [error.message]});
   }
