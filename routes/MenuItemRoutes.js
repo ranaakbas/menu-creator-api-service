f53722af93ce36f0592e0fa5907188cd69ea2e9f
Rana Akbas <akbasnar@gmail.com>
2024-07-14 15:22:58 +0300

    menuItemPriceHistory

--- routes/MenuItemRoutes.js
+++ routes/MenuItemRoutes.js
@@ -139,4 +139,21 @@ router.delete("/:id", async (req, res) => {
   }
 });
 
+router.get("/:id/price-history", async (req, res) => {
+  try {
+    const {id} = req.params;
+
+    const menuItem = await MenuItem.exists({_id: req.params.id, isDeleted: false});
+    if (!menuItem) {
+      return res.status(404).json({errors: ["menu item not found"]});
+    }
+    const priceHistory = await PriceHistory.find({menuItem: id});
+    const data = priceHistory.map(e => e.price);
+
+    return res.json(data);
+  } catch (error) {
+    return res.status(400).json({errors: [error.message]});
+  }
+});
+
 module.exports = router;
